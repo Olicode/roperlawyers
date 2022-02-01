e5201c48579b6d85257104061f785ae5f282b265
olivier roper <olivier.h.roper@gmail.com>
2022-02-01 16:37:32 +0800

    commit for poa

--- db/migrate/20220201064622_add_name_of_the_present_spouse_name_of_the_previous_spouses_date_of_divorce_date_of_decease_needs_poa_to_users.rb
+++ db/migrate/20220201064622_add_name_of_the_present_spouse_name_of_the_previous_spouses_date_of_divorce_date_of_decease_needs_poa_to_users.rb
@@ -4,6 +4,5 @@ class AddNameOfThePresentSpouseNameOfThePreviousSpousesDateOfDivorceDateOfDeceas
     add_column :users, :name_of_the_previous_spouses, :string
     add_column :users, :date_of_divorce, :date
     add_column :users, :date_of_decease, :date
-    add_column :users, :needs_poa, :string
   end
 end
