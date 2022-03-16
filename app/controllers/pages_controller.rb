49acea85ac4fc85212ec69f3627dd96d176cdd3f
olivier roper <olivier.h.roper@gmail.com>
2022-03-16 23:34:35 +0800

    added render reviews on each landing page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,11 @@
 class PagesController < ApplicationController
+  before_action :set_reviews, except: %i[contact_us]
+
   def lanzarote
   end
 
   def tenerife
+    @reviews = JSON.parse(File.read(File.join('public', 'tenerife_reviews.json')))
   end
 
   def review_page
@@ -12,12 +15,17 @@ class PagesController < ApplicationController
   end
 
   def home
-    @reviews = JSON.parse(File.read(File.join('public','reviews.json')))
   end
 
   def contact_us
     # TODO
     TransactionalMailer.contact_us('info@roperlawyers.com').deliver
     render json: { success: true }
+  end
+
+  private
+
+  def set_reviews
+    @reviews = JSON.parse(File.read(File.join('public','reviews.json')))
   end
 end
