ec329afb98b47d2f8f5d023ba2f3a7dbe5555f4f
olivier roper <olivier.h.roper@gmail.com>
2023-01-14 21:00:55 +0000

    fix schema

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,31 @@
 module ApplicationHelper
+  def pick_random_number_based_on_month
+    month = Date.current.month
+
+    if month == 2
+      8
+    elsif month == 2
+      5
+    elsif month == 3
+      4
+    elsif month == 4
+      5
+    elsif month == 5
+      3
+    elsif month == 6
+      5
+    elsif month == 7
+      4
+    elsif month == 8
+      5
+    elsif month == 9
+      3
+    elsif month == 10
+      4
+    elsif month == 11
+      5
+    elsif month == 12
+      4
+    end
+  end
 end
--- db/migrate/20220120035654_add_date_of_birth_expiry_date_to_users.rb
+++ db/migrate/20220120035654_add_date_of_birth_expiry_date_to_users.rb
@@ -1,6 +1,6 @@
 class AddDateOfBirthExpiryDateToUsers < ActiveRecord::Migration[6.1]
   def change
-    add_column :users, :date_of_, :date
+    add_column :users, :date_of_birth, :date
     add_column :users, :expiry_date, :date
   end
 end
