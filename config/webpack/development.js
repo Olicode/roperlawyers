6d93fcf3087999403f2f2bc72a59d70ae411f118
onwaxcomm@gmail.com <onwaxcomm@gmail.com>
2023-12-12 21:51:43 -0300

    adding the from in the front, designed not finished

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -2,4 +2,12 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 const environment = require('./environment')
 
+environment.config.merge({
+  node: {
+    __dirname: true,
+    __filename: true,
+    global: true,
+  },
+});
+
 module.exports = environment.toWebpackConfig()
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,11 +1,34 @@
 const { environment } = require('@rails/webpacker')
 
-const webpack = require('webpack')
-environment.plugins.prepend('Provide',
-new webpack.ProvidePlugin({
-    $: 'jquery/src/jquery',
-    jQuery: 'jquery/src/jquery'
-})
-)
+const customConfig = {
+  resolve: {
+    fallback: {
+      dgram: false,
+      fs: false,
+      net: false,
+      tls: false,
+      child_process: false,
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+};
 
-module.exports = environment
+environment.config.delete('node.dgram');
+environment.config.delete('node.fs');
+environment.config.delete('node.net');
+environment.config.delete('node.tls');
+environment.config.delete('node.child_process');
+
+environment.config.merge(customConfig);
+
+module.exports = environment;
