1a53afc3313c591b36b03ea7d15d700a36217718
smt4a <smt4a@github.com>
2024-03-11 19:45:18 +0000

    Contact Mailers

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -6,3 +6,14 @@ ActionMailer::Base.smtp_settings = {
   user_name:      'apikey',
   password:       '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'
 }
+
+ActionMailer::Base.default :from => ENV['MAILER_FROM'] || 'Roper Lawyers<info@roperlawyers.com>'
+
+ActionMailer::Base.smtp_settings = {
+  domain: 'roperlawyers.com',
+  address:        "smtp.postmarkapp.com",
+  port:            587,
+  authentication: :plain,
+  user_name:      'API_KEY',
+  password:       'API_KEY'
+}
