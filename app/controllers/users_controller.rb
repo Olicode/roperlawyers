13401a3ba3da1acbd8914063edcdc8a514f5cb1b
Jonathan Pike <jonathan.d.s.pike@gmail.com>
2025-07-03 13:25:37 -0600

    replace async Salesforce sync with direct service calls in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -75,7 +75,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        SalesforceSyncJob.perform_later(@user)
+        sync_user_with_salesforce
 
         format.html { redirect_to user_url(@user), notice: "User was successfully created." }
         format.json { render :show, status: :created, location: @user }
@@ -90,7 +90,7 @@ class UsersController < ApplicationController
   def update
     respond_to do |format|
       if @user.update(user_params)
-        SalesforceSyncJob.perform_later(@user.id, user_params.to_h)
+        sync_user_with_salesforce
 
         format.html { redirect_to user_url(@user), notice: "User was successfully updated." }
         format.json { render :show, status: :ok, location: @user }
@@ -115,6 +115,93 @@ class UsersController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_user
       @user = User.find(params[:id])
+    end
+    
+    def sync_user_with_salesforce
+      if @user.sf_contact_id.blank?
+        sf_contact_id = SalesforceService.create_or_update_contact(sf_attrs_map(@user))
+        @user.update!(sf_contact_id: sf_contact_id) unless sf_contact_id == false
+      else
+        SalesforceService.update_contact(sf_attrs_map(@user).merge!(Id: @user.sf_contact_id))
+  
+        if @user.nie_document.attached?
+          SalesforceService.upload_file(sf_file_upload_attrs_map(@user, @user.nie_document, "NIE"))
+        end
+  
+        if @user.passport_document.attached?
+          SalesforceService.upload_file(sf_file_upload_attrs_map(@user, @user.passport_document, "Passport"))
+        end
+  
+        if @user.igic_registration_modelo_400_document.attached?
+          SalesforceService.upload_file(sf_file_upload_attrs_map(@user, @user.igic_registration_modelo_400_document, "IGIC Registration Modelo 400"))
+        end
+  
+        @user.nota_simple_documents.each do |document|
+          begin
+            Rails.logger.info "Uploading Nota Simple document: #{document.inspect}"
+            SalesforceService.upload_file(sf_file_upload_attrs_map(@user, document, "Nota Simple"))
+            Rails.logger.info "Successfully uploaded Nota Simple document"
+          rescue => e
+            Rails.logger.error "Error uploading Nota Simple document: #{e.message}"
+            Rails.logger.error e.backtrace.join("\n")
+          end
+        end
+  
+        @user.title_deed_documents.each do |document|
+          begin
+            Rails.logger.info "Uploading Title Deed document: #{document.inspect}"
+            SalesforceService.upload_file(sf_file_upload_attrs_map(@user, document, "Title Deed"))
+            Rails.logger.info "Successfully uploaded Title Deed document"
+          rescue => e
+            Rails.logger.error "Error uploading Title Deed document: #{e.message}"
+            Rails.logger.error e.backtrace.join("\n")
+          end
+        end
+  
+        @user.vv_license_documents.each do |document|
+          SalesforceService.upload_file(sf_file_upload_attrs_map(@user, document, "VV License"))
+        end
+  
+        @user.first_occupation_license_documents.each do |document|
+          SalesforceService.upload_file(sf_file_upload_attrs_map(@user, document, "First Occupation License"))
+        end
+  
+        @user.cee_documents.each do |document|
+          SalesforceService.upload_file(sf_file_upload_attrs_map(@user, document, "CEE"))
+        end
+  
+        @user.civil_liability_insurance_policy_documents.each do |document|
+          SalesforceService.upload_file(sf_file_upload_attrs_map(@user, document, "Civil Liability Insurance Policy"))
+        end
+  
+        @user.habitability_certificate_documents.each do |document|
+          SalesforceService.upload_file(sf_file_upload_attrs_map(@user, document, "Habitability Certificate"))
+        end
+  
+        @user.municipal_certificate_documents.each do |document|
+          SalesforceService.upload_file(sf_file_upload_attrs_map(@user, document, "Municipal Certificate"))
+        end
+  
+        @user.property_tax_receipt_documents.each do |document|
+          SalesforceService.upload_file(sf_file_upload_attrs_map(@user, document, "Property Tax Receipt (IBI)"))
+        end
+  
+        @user.floor_plan_documents.each do |document|
+          SalesforceService.upload_file(sf_file_upload_attrs_map(@user, document, "Floor Plan"))
+        end
+  
+        @user.community_approval_documents.each do |document|
+          SalesforceService.upload_file(sf_file_upload_attrs_map(@user, document, "Community Approval"))
+        end
+  
+        @user.water_bill_documents.each do |document|
+          SalesforceService.upload_file(sf_file_upload_attrs_map(@user, document, "Water Bill"))
+        end
+  
+        @user.electricity_bill_documents.each do |document|
+          SalesforceService.upload_file(sf_file_upload_attrs_map(@user, document, "Electricity Bill"))
+        end
+      end
     end
 
     # Only allow a list of trusted parameters through.
