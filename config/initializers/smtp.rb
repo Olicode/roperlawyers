93e3227aa83d16dd6779e6c3c300c4125e04942e
Jonathan Pike <jonathan.pike@fullscript.com>
2026-01-27 16:16:30 -0700

    Restore postmark config

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,16 +1,10 @@
-# Use SendGrid for email delivery
-ActionMailer::Base.smtp_settings = {
-  domain: 'roperlawyers.com',
-  address:        "smtp.sendgrid.net",
-  port:            587,
-  authentication: :plain,
-  user_name:      'apikey',
-  password:       ENV['SENDGRID_API_KEY'] || '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'
-}
-
 ActionMailer::Base.default :from => 'Roper Lawyers<info@roperlawyers.com>'
 
-# Enable delivery
-ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.perform_deliveries = true
-ActionMailer::Base.raise_delivery_errors = true
+ActionMailer::Base.smtp_settings = {
+  domain: 'roperlawyers.com',
+  address:        "smtp.postmarkapp.com",
+  port:            587,
+  authentication: :plain,
+  user_name:      ENV['POSTMARK_API_KEY'],
+  password:       ENV['POSTMARK_API_KEY']
+}
