1385b27ede8d154b93bb50706d75e6aabcac760f
Olicode <olivier.h.roper@gmail.com>
2026-03-08 23:17:07 +0000

    Remove deleted channels import from application.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,7 +3,7 @@ import "@hotwired/turbo-rails";
 import { Application } from "@hotwired/stimulus";
 import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers";
 import Rails from "@rails/ujs";
-import "./channels";
+
 
 window.Stimulus = Application.start();
 const context = require.context("./controllers", true, /_controller\.js$/);
